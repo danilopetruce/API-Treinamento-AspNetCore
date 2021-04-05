06b17f37f8a588821e69b9d13a7f8872ccded6a9
Danilo Petruce Marques <danilopetruce@hotmail.com>
2021-04-05 02:14:08 -0300

    Controllers atualizados, e migrations OK

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,14 @@
+using System.Text;
+using Backoffice;
 using Backoffice.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace Shop
@@ -23,7 +27,30 @@ namespace Shop
     {
 
       services.AddControllers();
-      services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+
+      var key = Encoding.ASCII.GetBytes(Settings.Secret);
+      services.AddAuthentication(x =>
+      {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      })
+      .AddJwtBearer(x =>
+      {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey(key),
+          ValidateIssuer = false,
+          ValidateAudience = false
+        };
+      });
+
+      // services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+
+      services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+
       services.AddScoped<DataContext, DataContext>();
 
       services.AddSwaggerGen(c =>
@@ -45,6 +72,8 @@ namespace Shop
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseAuthentication();
 
       app.UseAuthorization();
 
